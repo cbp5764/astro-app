0b3761edac21b52f67d702eb3f2771ad646507dd
cbp5764 <83044251+cbp5764@users.noreply.github.com>
2026-08-28 17:31:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -33,8 +33,10 @@ function loadSessionData() {
         const y = radius * Math.cos(phi);
         const z = -radius * Math.sin(phi) * Math.cos(theta);
 
-        entity.setAttribute('position', `${x} ${y} ${z}`);
-        entity.setAttribute('rotation', `0 ${obj.azimuth} ${obj.rotation}`);
+        //entity.setAttribute('position', `${x} ${y} ${z}`);
+        entity.setAttribute('position', `0 0 -3`);
+        //entity.setAttribute('rotation', `0 ${obj.azimuth} ${obj.rotation}`);
+        entity.setAttribute('rotation', `0 0 0`);
         entity.setAttribute('width', '3');
         entity.setAttribute('height', '3');
 
